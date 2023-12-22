e1841689e1e40ba9cf736f38fb0166b0e9751717
Григорий Вихрев <ryhorvih@mail.ru>
2023-12-22 12:15:21 +0300

    Добавьте файлы проекта.

---
+++ BigData/MoviesDb.cs
@@ -0,0 +1,140 @@
+﻿using Azure.Core;
+using MoviesData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Concurrent;
+using System.ComponentModel.Design;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Net;
+
+
+namespace MoviesData
+{
+    public class MoviesDb
+    {
+        private static void ReacreateDb()
+        {
+            Parser parser = new Parser();
+            parser.ReadInfo();
+
+            using ApplicationContext db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Actors.AddRange(parser.actors.Values);
+            db.SaveChanges();
+            db.Tags.AddRange(parser.tags.Values);
+            db.SaveChanges();
+            db.Movies.AddRange(parser.movies.Values);
+            db.SaveChanges();
+        }
+        public static List<Movie> GenerateSimilarForMovie(Movie movie)
+        {
+            using var db = new ApplicationContext();
+
+
+            List<Movie> list = movie.Actors.SelectMany(actor => getActorById(actor.Id).Movies)
+                            .Union(movie.Tags.SelectMany(tag => getTagById(tag.Id).Movies))
+                            .Where(movie => !string.IsNullOrEmpty(movie.movieId))
+                            .DistinctBy(movie => movie.movieId)
+                            .ToList();
+
+            if (list ==null || list.Count == 1 || list.Count == 0)
+            {
+                return new();
+            }
+
+           
+            list.Remove(movie);
+
+            var Comparer = new MovieSimco(movie);
+            List<Movie> result = new();
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Movie? maxMovie = list[0];
+                foreach (var m in list)
+                {
+                    if (Comparer.Compare(m, maxMovie) == 1)
+                    {
+                        maxMovie = m;
+                    }
+                }
+                list.Remove(maxMovie);
+                result.Add(maxMovie);
+            }
+
+            return result;
+        }
+
+        public static Movie? getMovieById(string id)
+        {
+            using var db = new ApplicationContext();
+            return db.Movies.Where(m => m.movieId == id)
+                .Include(m => m.Actors)
+                .Include(m => m.Tags)
+                .FirstOrDefault();
+        }
+
+        
+        public static Actor? getActorById(string id)
+        {
+            using var db = new ApplicationContext();
+            return db.Actors.Where(a => a.Id == id)
+                .Include(a => a.Movies)
+                .FirstOrDefault();
+        }
+
+       
+        public static Tag? getTagById(string id)
+        {
+            using var db = new ApplicationContext();
+            return db.Tags.Where(t => t.Id == id)
+                .Include(t => t.Movies)
+                .FirstOrDefault();
+        }
+
+       
+        public static List<Movie> FindMovies(string name, int quantity)
+        {
+            using var db = new ApplicationContext();
+            return db.Movies
+                .Where(m => (m.NameRU + m.NameUS).ToLower().Contains(name))
+                .Take(quantity)
+                .ToList();
+        }
+
+        
+        public static List<Actor> FindActors(string name, int quantity)
+        {
+            using var db = new ApplicationContext();
+            return db.Actors
+                .Where(a => a.Name.ToLower().Contains(name.ToLower()))
+                .Take(quantity)
+                .ToList();
+        }
+
+       
+        public static List<Tag> FindTags(string name, int quantity)
+        {
+            using var db = new ApplicationContext();
+            return db.Tags
+                .Where(t => t.Name.ToLower().Contains(name.ToLower()))
+                .Take(quantity)
+                .ToList();
+        }
+
+        public static void Main() { }
+
+      
+
+    }
+
+
+}
---
+++ blazorprojj/Models/Movie.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesData.Models
+{
+    public class Movie
+    {
+        public string? NameRU { get; set; }
+        [NotMapped]
+        public string ImageUrl { get; set; }
+       
+        public string? NameUS { get; set; }
+        public List<Actor> Actors = new();
+        public List<Tag> Tags = new();
+        public double? Rate { get; set; }
+        public string? imdbId { get; set; }
+        [Key] public string movieId { get; set; }
+
+
+        
+        public double Similarity(Movie movie)
+        {
+            double actorsIntersect = Actors.Select(a => a.Id)
+                .Intersect(movie.Actors.Select(a => a.Id))
+                .Count();
+            
+            double actorsUnion = Actors.Count;
+
+            double tagsIntersect = Tags.Select(t => t.Id)
+                .Intersect(movie.Tags.Select(t => t.Id))
+                .Count();
+            
+            double tagsUnion = Tags.Count;
+
+            double actorsRate; 
+            if (actorsUnion != 0)
+                actorsRate = actorsIntersect / actorsUnion;
+            else
+                actorsRate = 0;
+
+            double tagsRate; 
+            if (tagsUnion != 0)
+                tagsRate = tagsIntersect / tagsUnion;
+            else
+                tagsRate = 0;
+
+            double rate = (actorsRate + tagsRate) * 0.5;
+
+            double anotherMovieRate = 0; 
+
+            if (movie.Rate != null)
+            {
+                anotherMovieRate = (double)movie.Rate;
+            }
+
+            
+            double resultRate = rate * 0.9 + (anotherMovieRate * 0.1) * 0.1;
+            
+
+            return resultRate;
+        }
+        public Movie()
+        {
+
+        }
+        public async void GetMovieImageUrl()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string url = $"https://www.omdbapi.com/?i={imdbId}&apikey=c45ce5e";
+                string info = client.DownloadString(url);
+                int start = info.IndexOf("https://");
+                int end = info.IndexOf(".jpg")+4;
+                ImageUrl = info.Substring(start,end-start); 
+
+            }
+        }
+    }
+    public class MovieSimco : IComparer<Movie>
+    {
+        public Movie mainMovie { get; private set; }
+        public MovieSimco(Movie mainMovie)
+        {
+            this.mainMovie = mainMovie;
+        }
+        public int Compare(Movie? x, Movie? y)
+        {
+            if (mainMovie.Similarity(x) < mainMovie.Similarity(y))
+                return -1;
+            else if (mainMovie.Similarity(x) > mainMovie.Similarity(y))
+                return 1;
+            else
+                return 0;
+        }
+       
+    }
+}
